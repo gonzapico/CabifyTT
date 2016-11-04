30714163302ec31ec3b002b2d4607e568059635d
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2016-11-04 09:25:29 +0100

    Added request API with Retrofit

--- data/src/main/java/xyz/gonzapico/data/cloud/CabifyAPI.java
+++ data/src/main/java/xyz/gonzapico/data/cloud/CabifyAPI.java
@@ -1,7 +1,9 @@
 package xyz.gonzapico.data.cloud;
 
+import java.util.List;
 import retrofit2.Response;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 import xyz.gonzapico.data.entity.EstimateVehicle;
 import xyz.gonzapico.data.entity.StopsBodyRequest;
@@ -11,6 +13,9 @@ import xyz.gonzapico.data.entity.StopsBodyRequest;
  */
 
 public interface CabifyAPI {
-  @POST("/api/v2/estimate") rx.Observable<Response<EstimateVehicle>> estimate(
+  @Headers({
+      "Content-type: application/json",
+      "Authorization: Bearer 6o_FrppOEQ5RrCkBOEKaBM-puJleMKrRn5nW_cy7H9Y", "Accept-Language: en"
+  }) @POST("/api/v2/estimate") rx.Observable<Response<List<EstimateVehicle>>> estimate(
       @Body StopsBodyRequest body);
 }
