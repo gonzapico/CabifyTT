b9e03a1c5348515ad8a3aba43caf09e1a4193d21
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2016-11-04 09:26:54 +0100

    Added DI of the components of the data layer

---
+++ data/src/main/java/xyz/gonzapico/data/di/CloudModule.java
@@ -0,0 +1,105 @@
+package xyz.gonzapico.data.di;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import dagger.Module;
+import dagger.Provides;
+import java.io.IOException;
+import javax.inject.Singleton;
+import okhttp3.Cache;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import xyz.gonzapico.data.cloud.CabifyAPI;
+
+/**
+ * Created by gfernandez on 28/07/16.
+ */
+@Module public class CloudModule {
+
+  String mBaseUrl;
+  Context context;
+
+  // Constructor needs one parameter to instantiate.
+  public CloudModule(String baseUrl, Context context) {
+    this.mBaseUrl = baseUrl;
+    this.context = context;
+  }
+
+  @Provides @Singleton Gson provideGson() {
+    GsonBuilder gsonBuilder = new GsonBuilder();
+    gsonBuilder.setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES);
+    return gsonBuilder.create();
+  }
+
+  @Provides @Singleton Retrofit provideRetrofit(Gson gson, OkHttpClient.Builder okHttpBuilder) {
+
+    Retrofit retrofit =
+        new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create(gson))
+            .baseUrl(mBaseUrl)
+            .client(okHttpBuilder.build())
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build();
+
+    retrofit.create(CabifyAPI.class);
+
+    return retrofit;
+  }
+
+  @Provides @Singleton Cache provideOkHttpCache() {
+    int cacheSize = 10 * 1024 * 1024; // 10 MiB
+    Cache cache = new Cache(this.context.getCacheDir(), cacheSize);
+    return cache;
+  }
+
+  @Provides @Singleton OkHttpClient.Builder provideOkHttpClient(Cache cache) {
+    OkHttpClient.Builder client = new OkHttpClient.Builder();
+
+    // Logging porpouses
+    HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
+    logging.setLevel(HttpLoggingInterceptor.Level.BODY);
+    client.addInterceptor(logging);
+    client.networkInterceptors().add(new Interceptor() {
+      @Override public Response intercept(Chain chain) throws IOException {
+        Response originalResponse = chain.proceed(chain.request());
+        if (isThereInternetConnection()) {
+          int maxAge = 60; // read from cache for 1 minute
+          return originalResponse.newBuilder()
+              .header("Cache-Control", "public, max-age=" + maxAge)
+              .build();
+        } else {
+          int maxStale = 60 * 60 * 24 * 28; // tolerate 4-weeks stale
+          return originalResponse.newBuilder()
+              .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+              .build();
+        }
+      }
+    });
+    client.cache(cache);
+    return client;
+  }
+
+  /**
+   * Checks if the device has any active internet connection.
+   *
+   * @return true device with internet connection, otherwise false.
+   */
+  private boolean isThereInternetConnection() {
+    boolean isConnected;
+
+    ConnectivityManager connectivityManager =
+        (ConnectivityManager) this.context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+    isConnected = (networkInfo != null && networkInfo.isConnectedOrConnecting());
+
+    return isConnected;
+  }
+}
