be6587f2f94713e8a717c042eb37e11cff931740
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2016-11-05 10:49:01 +0100

    Added new entity

---
+++ data/src/main/java/xyz/gonzapico/data/entity/Stop.java
@@ -0,0 +1,10 @@
+package xyz.gonzapico.data.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.Generated;
+
+@Generated("org.jsonschema2pojo") public class Stop {
+
+  public List<Long> loc = new ArrayList<>();
+}
