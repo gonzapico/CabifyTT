ac8c61d0b5ff47224b6b8e885cf5df1801ee2f0d
Gonzalo Fernández <gonzalo.fernandez.pico@gmail.com>
2016-11-05 10:53:54 +0100

    Set the correct method corresponding to the request/response of the API

--- domain/src/main/java/xyz/gonzapico/domain/repository/EstimateDomainRepository.java
+++ domain/src/main/java/xyz/gonzapico/domain/repository/EstimateDomainRepository.java
@@ -1,10 +1,16 @@
 package xyz.gonzapico.domain.repository;
 
+import java.util.List;
+import rx.Observable;
+import xyz.gonzapico.domain.model.DomainModelBodyRequestStops;
+import xyz.gonzapico.domain.model.DomainModelEstimateVehicle;
+
 /**
  * Created by gfernandez on 3/11/16.
  */
 
 public interface EstimateDomainRepository {
 
-  void getEstimationVehicelList();
+  Observable<List<DomainModelEstimateVehicle>> getEstimationVehicleList(
+      DomainModelBodyRequestStops bodyRequestStops);
 }
